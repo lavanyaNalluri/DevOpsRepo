2c1d73a67cf65152ad624843057c1017c90e84f0
Lavanya Nalluri <nalluri.lavanya09@gmail.com>
2021-02-23 17:33:29 +0530

    First time commiting and pushing the code to github

---
+++ NetSuitePythonProject/src/InitialPackage/InitialModule.py
@@ -0,0 +1,15 @@
+'''
+Created on 17-Feb-2021
+
+@author: Lavanya Nalluri
+'''
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+user_name = "lavanya.nalluri@cigniti.com"
+password = "NetSuite2020@"
+driver = webdriver.Firefox()
+driver.get("https://system.na0.netsuite.com/pages/customerlogin.jsp")
+
+
+
+
---
+++ NetSuitePythonProject/src/InitialPackage/test_demo.py
@@ -0,0 +1,5 @@
+#Any pytest file should start with test_ or end with _test
+#pytest method names should start with test 
+
+def test_firstProgram():
+    print("HEllo")
